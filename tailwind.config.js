c30bcb861ba37d7fa2d8e834a775b68169563e78
Pouya-Khani <pooyakhorshidkhani@gmail.com>
2024-12-25 22:11:07 +0330

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./public/**/*.{html,js}"],
+	theme: {
+		extend: {
+			spacing: {
+				18: "4.5rem",
+			},
+		},
+	},
+	plugins: [],
+};
